0f7cc2534d5368cbc4e82bacc37cd6c96f858b84
sumh <963455713@qq.com>
2018-06-21 14:34:52 +0800

    第一次修改

---
+++ FirstGitDemo/src/com/j1801/app/FirstGitDemo.java
@@ -0,0 +1,10 @@
+package com.j1801.app;
+
+public class FirstGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("第一次更改");
+	}
+
+}
